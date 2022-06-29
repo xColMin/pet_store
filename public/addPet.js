b0bf0712b7d4405d0880830ffb64ba5ae78d905c
Pablo Sierra <pablo.sierra@lekta.ai>
2022-06-29 10:49:04 +0800

    pets data completed

--- public/addPet.js
+++ public/addPet.js
@@ -1,20 +1,21 @@
 async function addPet() {
   let data = document.getElementById("b64").textContent;
-
-  console.log("DATAAAAAAAA");
-  console.log(typeof data);
+  let category = document.getElementById("category").value;
+  let price = document.getElementById("price").value;
+  let quantity = document.getElementById("quantity").value;
 
   let id = Date.now();
   let name = document.getElementById("petname").value;
   const pet = {
     "category": {
-      "id": id,
-      "name": name,
+      "id": category,
+      "name": getCategory(category),
     },
     "name": name,
+    "price": price,
     "photoUrls": [{ data }],
     "tags": [{ "id": id, "name": name }],
-    "status": "available",
+    "status": [{ "available": quantity, "pending": 0, "sold": 0 }],
   };
 
   const options = {
@@ -28,5 +29,20 @@ async function addPet() {
 
   const jsonResponse = await fetch("/pet_api", options);
   const jsonData = await jsonResponse.json();
-  //window.location.href = "index.html";
+  const m = document.getElementById("message");
+  m.textContent = "Pet Added!";
+}
+
+function getCategory(category) {
+  if (category == 1) {
+    return "Dog";
+  } else if (category == 2) {
+    return "Cat";
+  } else if (category == 3) {
+    return "Parrot";
+  } else if (category == 4) {
+    return "Mice";
+  } else if (category == 5) {
+    return "Rabbit";
+  }
 }
