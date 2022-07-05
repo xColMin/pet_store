db35a4ce6413d5eeefd7f415b23f5c53295afc5f
Pablo Sierra <pablo.sierra@lekta.ai>
2022-07-05 14:57:05 +0800

    add pet refactor

--- public/addPet.js
+++ public/addPet.js
@@ -43,3 +43,18 @@ function getCategory(category) {
     return "Rabbit";
   }
 }
+
+function readFile() {
+  if (!this.files || !this.files[0]) return;
+
+  const FR = new FileReader();
+
+  FR.addEventListener("load", function (evt) {
+    document.querySelector("#img").src = evt.target.result;
+    document.querySelector("#b64").textContent = evt.target.result;
+  });
+
+  FR.readAsDataURL(this.files[0]);
+}
+
+document.querySelector("#inp").addEventListener("change", readFile);
